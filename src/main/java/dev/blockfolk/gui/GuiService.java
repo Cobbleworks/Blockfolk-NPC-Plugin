5cba6bba9d39a4f1fbaa1fe850a7d312d9a5b1c7
Andreas Hagen <andreas.hagen@outlook.com>
2026-07-13 20:26:13 +0200

    Shift + left-click: spawn a persistent copy at your current location.

--- src/main/java/dev/blockfolk/gui/GuiService.java
+++ src/main/java/dev/blockfolk/gui/GuiService.java
@@ -294,6 +294,7 @@ public final class GuiService implements Listener {
                     ChatColor.GRAY + "Instances: " + ChatColor.WHITE + instances,
                     statusLine(definition),
                     ChatColor.YELLOW + "Click to manage",
+                    ChatColor.AQUA + "Shift + left-click to spawn a copy here",
                     ChatColor.RED + "Shift + right-click to delete"
             )));
         }
@@ -948,6 +949,11 @@ public final class GuiService implements Listener {
             NpcDefinition definition = definitions.get(index);
             if (event.getClick() == ClickType.SHIFT_RIGHT) {
                 openConfirmation(player, definition, ConfirmationAction.DELETE_DEFINITION, page);
+            } else if (event.getClick() == ClickType.SHIFT_LEFT) {
+                instanceRegistry.spawnPersistent(definition, player.getLocation());
+                player.sendMessage(Component.text("Spawned NPC copy of " + definition.getDisplayName()
+                        + " at your location."));
+                openMain(player, page);
             } else {
                 openEditor(player, definition);
             }
